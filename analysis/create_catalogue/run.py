bf3a16e5734312233b0f769aebd5a972d10eb086
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2022-09-26 17:18:29 +0100

    updated

--- analysis/create_catalogue/run.py
+++ analysis/create_catalogue/run.py
@@ -174,7 +174,7 @@ if __name__ == "__main__":
     grid = SpectralGrid(grid_name)
 
     z = 10.0
-    N = 10000
+    N = 10
 
     # --- calculate broadband luminosities
     filters = [f'JWST/NIRCam.{f}' for f in ['F090W', 'F115W','F150W','F200W','F277W','F356W','F410M','F444W']] # define a list of filter codes
