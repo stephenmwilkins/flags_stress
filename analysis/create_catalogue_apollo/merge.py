af99d181470784b645e119a940b80383981aecb6
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2022-09-27 07:59:38 +0100

    added merge.py

--- analysis/create_catalogue_apollo/merge.py
+++ analysis/create_catalogue_apollo/merge.py
@@ -5,10 +5,10 @@ import numpy as np
 import h5py
 
 
-keys2 = []
+keys = []
 
 def append(k):
-    keys2.append(k)
+    keys.append(k)
 
 
 
@@ -21,43 +21,37 @@ if __name__ == "__main__":
 
     # --- calculate length of output array
     N = 0
-    #for i in range(1000):
-    #    z = 7. + (0.01 * i)
-
-    #    with h5py.File(f'out/{scenario}/{z:.2f}.hf', 'r') as hf:
-    #        N += len(hf['z'])
+    for i in range(1000):
+        z = 7. + (0.01 * i)
+        with h5py.File(f'out/{scenario}/{z:.2f}.hf', 'r') as hf:
+            N += len(hf['parameters/z'])
 
     print(f'total number of objects: {N}')
 
+
+    # --- make a list of keys
     z = 7.00
-
     with h5py.File(f'out/{scenario}/{z:.2f}.hf', 'r') as hf:
-
-        keys = list(hf.keys())
         hf.visit(append)
 
-    print(keys)
-    print(keys2)
+    for k in keys:
+        print(k)
+        hfo[k] = np.empty(N)
 
 
+    rn = 0
 
-    #
-    #
-    # o = {k: np.empty(N) for k in keys}
-    #
-    # print(o)
-    #
-    # rn = 0
-    #
-    # for z in np.arange(5., 16., 1.):
-    #
-    #     input = h5py.File(f'{t}_{z}.h5', 'r')
-    #
-    #     n = len(input['z'])
-    #     print(rn, n)
-    #     for k in keys:
-    #         print(len(input[k][()]))
-    #
-    #         o[k][rn:rn+n] = input[k][()]
-    #
-    #     rn += n
+    for i in range(1000):
+
+        z = 7. + (0.01 * i)
+
+        with h5py.File(f'out/{scenario}/{z:.2f}.hf', 'r') as hf:
+
+            n += len(hf['parameters/z'])
+
+            for k in keys:
+                hfo[k][rn:rn+n] = hf[k][()]
+
+            rn += n
+
+    hfo.flush()
